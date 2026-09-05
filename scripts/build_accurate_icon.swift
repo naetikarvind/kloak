454e1205008718dfa788a8030c6f6bef00bff3c4
naetikarvind <154405231+naetikarvind@users.noreply.github.com>
2026-09-05 16:12:59 +0530

    fix(icons): render all 4 icon layers including glowing white K emblem on front of shield

--- scripts/build_accurate_icon.swift
+++ scripts/build_accurate_icon.swift
@@ -1,3 +1,7 @@
+import Foundation
+import AppKit
+
+let masterSvg = """
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" width="1024" height="1024">
   <defs>
     <!-- Background Squircle Gradient -->
@@ -80,3 +84,88 @@
     </g>
   </g>
 </svg>
+"""
+
+func renderPng(svgString: String, size: CGFloat, targetUrl: URL) -> Bool {
+    guard let svgData = svgString.data(using: .utf8),
+          let image = NSImage(data: svgData) else {
+        print("Failed to load NSImage from SVG data")
+        return false
+    }
+
+    let rep = NSBitmapImageRep(
+        bitmapDataPlanes: nil,
+        pixelsWide: Int(size),
+        pixelsHigh: Int(size),
+        bitsPerSample: 8,
+        samplesPerPixel: 4,
+        hasAlpha: true,
+        isPlanar: false,
+        colorSpaceName: .deviceRGB,
+        bytesPerRow: 0,
+        bitsPerPixel: 0
+    )
+
+    guard let bitmapRep = rep else { return false }
+    bitmapRep.size = NSSize(width: size, height: size)
+
+    NSGraphicsContext.saveGraphicsState()
+    let context = NSGraphicsContext(bitmapImageRep: bitmapRep)
+    NSGraphicsContext.current = context
+
+    image.draw(in: NSRect(x: 0, y: 0, width: size, height: size), from: .zero, operation: .copy, fraction: 1.0)
+
+    NSGraphicsContext.restoreGraphicsState()
+
+    guard let pngData = bitmapRep.representation(using: .png, properties: [:]) else {
+        return false
+    }
+
+    do {
+        try pngData.write(to: targetUrl)
+        return true
+    } catch {
+        print("Failed writing to \(targetUrl): \(error)")
+        return false
+    }
+}
+
+let fm = FileManager.default
+let currentDir = URL(fileURLWithPath: fm.currentDirectoryPath)
+let iconsetDir = currentDir.appendingPathComponent("AppIcon.iconset")
+try? fm.createDirectory(at: iconsetDir, withIntermediateDirectories: true)
+
+let sizes: [(String, CGFloat)] = [
+    ("icon_16x16.png", 16),
+    ("icon_16x16@2x.png", 32),
+    ("icon_32x32.png", 32),
+    ("icon_32x32@2x.png", 64),
+    ("icon_128x128.png", 128),
+    ("icon_128x128@2x.png", 256),
+    ("icon_256x256.png", 256),
+    ("icon_256x256@2x.png", 512),
+    ("icon_512x512.png", 512),
+    ("icon_512x512@2x.png", 1024)
+]
+
+for (name, size) in sizes {
+    let outUrl = iconsetDir.appendingPathComponent(name)
+    _ = renderPng(svgString: masterSvg, size: size, targetUrl: outUrl)
+}
+
+print("Rendered all iconset sizes!")
+
+// Save extension and resources
+let extIconsDir = currentDir.appendingPathComponent("packages/browser-extension/icons")
+try? fm.createDirectory(at: extIconsDir, withIntermediateDirectories: true)
+_ = renderPng(svgString: masterSvg, size: 16, targetUrl: extIconsDir.appendingPathComponent("icon-16.png"))
+_ = renderPng(svgString: masterSvg, size: 48, targetUrl: extIconsDir.appendingPathComponent("icon-48.png"))
+_ = renderPng(svgString: masterSvg, size: 128, targetUrl: extIconsDir.appendingPathComponent("icon-128.png"))
+
+let resDir = currentDir.appendingPathComponent("packages/macos-app/Sources/KloakApp/Resources")
+try? fm.createDirectory(at: resDir, withIntermediateDirectories: true)
+try? masterSvg.data(using: .utf8)?.write(to: resDir.appendingPathComponent("KloakIcon.svg"))
+_ = renderPng(svgString: masterSvg, size: 512, targetUrl: resDir.appendingPathComponent("AppIcon.png"))
+_ = renderPng(svgString: masterSvg, size: 512, targetUrl: currentDir.appendingPathComponent("accurate_icon.png"))
+
+print("Successfully rendered master SVG and all icon targets!")
